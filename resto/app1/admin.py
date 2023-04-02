52ade1bfd52b52e93616a6d89c92836696fafa47
juba02 <youvasin02@gmail.com>
2023-04-02 02:44:23 +0200

    modification du models.py + admin.py

--- resto/app1/admin.py
+++ resto/app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Consomation, Restaurateur
+
 # Register your models here.
+
+admin.site.register(Restaurateur)
+admin.site.register(Consomation)
--- resto/app1/models.py
+++ resto/app1/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Restaurateur(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    resto_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'resto_name', 'phone_number']
+
+    def __str__(self):
+        return self.resto_name
+
+class Menu(models.Model):
+    restaurateur=models.ForeignKey('Restaurateur', on_delete=models.CASCADE, null=True, blank=True)
+    consomation=models.ForeignKey('Consomation', on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Consomation(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
