ed180cf5b4ece0596f9ed98449438ba5db38c3cb
aziz <azizsellal.dz@gmail.com>
2023-04-11 03:13:48 +0100

    product

--- resto/app1/models.py
+++ resto/app1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    description = models.TextField(blank=True)
+    thubnail = models.ImageField(upload_to="products", blank=True, null=True)
